7eda78b1e0a7cf3444207b52483681a489456208
Samuel Ayomide <samuelayo61@gmail.com>
2024-07-27 08:16:05 +0100

    Added

--- src/pages/dashboard/gallery/GalleryPage.tsx
+++ src/pages/dashboard/gallery/GalleryPage.tsx
@@ -11,8 +11,8 @@ import EventGrid from "../../../components/grid/EventGrid";
 const GalleryPage = () => {
 
     const { notifyUser } = Toast();
-    const {   data, isError, isLoading } = useQuery("galleryData", fetchAllGalleryData);
-    console.log(data.data)
+    const { data, isError, isLoading } = useQuery("galleryData", fetchAllGalleryData);
+    console.log(data?.data)
    
   if (isLoading){
     return <CircleLoader />
