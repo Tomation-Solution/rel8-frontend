ca47cfdafd0b6ebcd148ac045bab30de3c0b01fc
Samuel Ayomide <samuelayo61@gmail.com>
2024-08-01 17:28:47 +0100

    Added

--- src/components/homepage/HomePageNotification.tsx
+++ src/components/homepage/HomePageNotification.tsx
@@ -12,9 +12,10 @@ const HomePageNotification = ({notificationItem}:Props) => {
   return (
     <div  className='bg-[#f9f9f9] border border-[#ececec] flex items-center gap-2 px-3 py-2 my-1 rounded-md' >
     <img className="w-7 h-7" src={notificationItem.latest_update_table_name === 'news' ? clockIcon : calendarIcon } alt="notification-image" />
-    <div className="grid " >
+    {/* <div className="grid " > */}
+    <div className="flex flex-col flex-grow">
         <span className="font-bold col-span-2 text-primary-blue capitalize" >{notificationItem.title}</span>
-        <div className="flex items-center gap-2 md:gap-5" >
+        <div className="flex justify-between gap-2" >
         <span className="text-xs col-span-2 text-[#3a3a3a] line-clamp-1" >{notificationItem.body}</span>
         {notificationItem.created_on && (
             <span className="whitespace-nowrap text-xs">{formatDate(notificationItem.created_on)}</span>
