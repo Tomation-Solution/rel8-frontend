a767c1bfb8c77ceef5bc247b0c2c87f3e8338498
Samuel Ayomide <samuelayo61@gmail.com>
2024-08-09 16:15:23 +0100

    Added to the event details page

--- src/pages/dashboard/events/EventDetailPage.tsx
+++ src/pages/dashboard/events/EventDetailPage.tsx
@@ -123,13 +123,13 @@ const EventDetailPage = () => {
     if (event){
 
         return (
-          <main className="grid grid-cols-4 gap-x-[60px] text-textColor ">
+          <main className="grid md:grid-cols-4 md:gap-10 gap-[50px] md:px-0 px-5 text-textColor ">
             {
               handleFreeEventMutation.isLoading?
               <CircleLoader />:''
             }
           <div className="col-span-3">
-            <BreadCrumb title="Events" />
+            <BreadCrumb title="Event's Details" />
             <div className="relative " >
                 <div className="relative flex items-center  h-[40vh]">
 
@@ -140,18 +140,18 @@ const EventDetailPage = () => {
                 alt=""
               />
                 </div>
-              <div className="grid grid-cols-5 my-5 gap-1">
+              <div className="grid md:grid-cols-5 grid-cols-3 my-5 gap-1">
                 <div className=" col-span-3 flex items-center font-semibold">
                
                   <span className="block" >Name of event: 
                   <span className="font-light text-justify" >
 
-                   {event.name} 
+                   {" "}{event.name} 
                   
                   </span>
                    </span>
                 </div>
-                <div className=" col-span-2 grid grid-cols-2 gap-3 h-fit">
+                <div className=" col-span-2 grid grid-cols-1 md:grid-cols-2 gap-3 h-fit">
                   <span className="flex items-center whitespace-nowrap py-3 px-2 text-sm bg-neutral-3 text-textColor rounded-md gap-2">
                     <img src={eventsIcon} className="w-6 h-6" alt="" />{" "}
                     <span>{event.startDate}</span>
@@ -163,7 +163,8 @@ const EventDetailPage = () => {
                 </div>
               </div>
             </div>
-            <div className="grid grid-cols-2 my-5 border-t border-b py-4">
+
+            <div className="grid grid-cols-1 md:grid-cols-2 md:gap-0 gap-3 my-5 border-t border-b py-4">
               <div className="flex items-center gap-2">
                 <img
                   src={event?.organiserImage ? event.organiserImage : dummyOrganizerImage}
@@ -172,36 +173,38 @@ const EventDetailPage = () => {
                 />
                 <div>
                   <h3 className="text-base ">
-                   { event?.organzier_name? event?.organzier_name:"Not Available"} <span className="text-xs">~ Organizer</span>{" "}
+                   { event?.organiser_name? event?.organiser_name:"Not Available"} <span className="text-xs">~ Organizer</span>{" "}
                   </h3>
                   <p className="text-sm text-neutral1">
                     {event?.organiser_extra_info ? event.organiser_extra_info : "Organiser Info not available"}
                   </p>
                 </div>
               </div>
+
               <Link to={`${event?.event_docs}`} className="bg-primary-blue text-center grid place-items-center text-white  border border-white h-[40px] rounded-md  hover:border-primary-blue">
                 Download Event Atachment
               </Link>
             </div>
+            
             <div className="" >
-                <h3 className="text-sm" >Event Fee: <span className="font-bold text-sm" >₦{event?.amount ?  parseFloat(event.amount).toFixed(2) :  "Not Available"}</span></h3>
+              <h3 className="text-sm">Event Fee: <span className="font-bold text-sm">
+                  {event?.amount && parseFloat(event.amount) > 0 ? `₦${parseFloat(event.amount).toFixed(2)}` : "Free"}
+                  </span>
+              </h3>
                 <div className="grid grid-cols-2 gap-2 my-3 text-sm">
                 <button className="bg-primary-blue text-white  border border-white h-[40px] rounded-md  ">
                Add Participants
               </button>
               {/* <Button type="outlined"  text="xjnasj" className="!h-[40px] grid  place-items-center" />  */}
               
-                <button onClick={event?.is_paid_event ? handleRegisterUserForPaidEvents :handleRegisterUserForFreeEvents}  className="bg-white text-primaryBlue border border-primary-blue h-[40px] rounded-md">
-               Register
-               {
-                event?.is_paid_event?"Pay for event":"Register for free"
-               }
-              </button>
+              <button onClick={event?.is_paid_event ? handleRegisterUserForPaidEvents : handleRegisterUserForFreeEvents} className="bg-white text-primaryBlue border border-primary-blue h-[40px] rounded-md">
+                                {event?.is_paid_event ? "Pay for event" : "Register for free"}
+                            </button>
                 </div>
             </div>
           </div>
-          <div className="hidden xl:inline col-span-1">
-            <SeeAll title="Others" path="/gallery" />
+          <div className="md:col-span-1 col-span-3">
+            <SeeAll title="Others" path="/events" />
             <EventGrid heightOfCard="h-[160px]" numberOfItemsToShow={3} />
             {/* <GalleryColumn numberOfItemsToShow={4} /> */}
           </div>
