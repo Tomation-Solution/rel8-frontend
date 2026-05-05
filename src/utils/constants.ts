8e15082c78a650e0830cddbd105b0b0e2a1d4077
Abdullah Ayoola <officialayo540@gmail.com>
2026-05-05 20:18:18 +0100

    code update

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,7 +8,7 @@ import { apiPublic } from "../api/baseApi";
 
 // export const ENDPOINT_URL = "https://rel8.watchdoglogisticsng.com";
 // export const ENDPOINT_URL = "http://localhost:8080";
-export const ENDPOINT_URL = import.meta.env.VITE_ENDPOINT_URL ?? "http://localhost:8080";
+export const ENDPOINT_URL = import.meta.env.VITE_ENDPOINT_URL ?? "https://rel8.watchdoglogisticsng.com";
 export const TENANT = getSubdomain() || "aani";
 export const WSS = `ws://rel8.watchdoglogisticsng.com/ws/chat/${TENANT}/`;
 export const sitename = "rel8.watchdoglogisticsng.com";
